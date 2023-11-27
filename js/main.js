c3744ba8c622fd07daae20fca0a601762128bbda
fantom030983 <ihorkavun1234@gmail.com>
2023-11-27 17:27:30 +0200

    HW Random Block

--- js/main.js
+++ js/main.js
@@ -1,64 +1,20 @@
-const square = document.getElementById('square');
-const body = document.body;
-const bodyWidth = body.clientWidth;
-const squareWidth = square.clientWidth;
-let positionSquare = 0;
+const blockRandom = document.querySelector('.block');
+// console.log(blockRandom)
 
-document.addEventListener('keydown', (event) => {
-  if (event.key === 'ArrowLeft') {
-    if (positionSquare > 0) {
-      positionSquare -= 10;
-      square.style.left = parseInt(square.style.left) - 10 + 'px';
-    }
-  } else if (event.key === 'ArrowRight') {
-    if (positionSquare < bodyWidth - squareWidth) {
-      positionSquare += 10;
-      square.style.left = parseInt(square.style.left) + 10 + 'px';
-    }
-  }
-});
-
-const bumperSquare = () => {
-  if (positionSquare + squareWidth >= bodyWidth ) {
-    positionSquare = bodyWidth - squareWidth - 20;
-    setTimeout(() => {
-      square.innerHTML = 'БЕМС';
-      setTimeout(() => {
-        square.innerHTML = '';
-      }, 2000);
-    }, 0);
-
-  } else if (positionSquare <= 0) {
-    positionSquare = 20;
-    setTimeout(() => {
-      square.innerHTML = 'БЕМС';
-      setTimeout(() => {
-        square.innerHTML = '';
-      }, 2000);
-    }, 0);
-    
-  }
-  square.style.left = positionSquare + 'px';
-}
-
-setInterval(bumperSquare);
+setInterval(() => {
+  const randomColorBlock = Math.floor(Math.random()*16777215).toString(16);
+  blockRandom.style.backgroundColor = "#" + randomColorBlock;
+}, 500);
 
 let positionX = 0;
 let positionY = 0;
 
-document.addEventListener('keydown', (event) => {
-  if (event.key === " ") {
-    positionY -= 10;
-    square.style.transform = `translate(${positionX}px, ${positionY}px)`;
-    setTimeout(() => {
-      positionY += 10;
-      square.style.transform = `translate(${positionX}px, ${positionY}px)`;
-      square.style.transition = '0.5s';
-    }, 200);
-  } else if (event.ctrlKey) {
-    square.style.height = square.offsetHeight * 0.6 + 'px';
-    square.style.width = square.offsetWidth * 1.25 + 'px';
-    square.style.transition = '0.5s';
-  };
-}
-);
+setInterval(() =>{
+    const windowWidth = window.innerWidth;
+    const windowHeight = window.innerHeight;
+    positionX = Math.floor(Math.random()*(windowWidth - blockRandom.clientWidth));
+    positionY = Math.floor(Math.random()*(windowHeight - blockRandom.clientHeight));
+
+    blockRandom.style.left = positionX + 'px';
+    blockRandom.style.top = positionY + 'px';
+}, 1000)
